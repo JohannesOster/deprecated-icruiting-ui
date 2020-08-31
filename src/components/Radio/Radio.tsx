033f6f223c694d55a923eb3bf3961e6b69b5cdd2
Johannes Oster <johannes.oster@outlook.at>
2020-08-31 15:25:51 +0200

    Wrap Radio and Select components into forwardRef

--- src/components/Radio/Radio.tsx
+++ src/components/Radio/Radio.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {forwardRef} from 'react';
 import {
   Container,
   Label,
@@ -9,31 +9,39 @@ import {
 } from './Radio.sc';
 import {RadioProps} from './types';
 
-export const Radio: React.FC<RadioProps> = ({
-  label,
-  description,
-  options,
-  errors = [],
-  ...props
-}) => {
-  const _errors = errors.map((error, idx) => <span key={idx}>• {error}</span>);
+const Radio: React.FC<RadioProps> = forwardRef<HTMLInputElement, RadioProps>(
+  ({label, description, options, errors = [], ...props}, ref) => {
+    const _errors = errors.map((error, idx) => (
+      <span key={idx}>• {error}</span>
+    ));
 
-  return (
-    <Container>
-      <Label htmlFor={props.name} error={!!errors.length}>
-        {label}
-        {props.required && '*'}
-      </Label>
-      <Description error={!!errors.length}>{description}</Description>
-      {options.map(({label, value}, idx) => {
-        return (
-          <OptionContainer key={idx}>
-            <input style={{margin: 0}} type="radio" value={value} {...props} />
-            <OptionLabel>{label}</OptionLabel>
-          </OptionContainer>
-        );
-      })}
-      <Errors>{_errors}</Errors>
-    </Container>
-  );
-};
+    return (
+      <Container>
+        <Label htmlFor={props.name} error={!!errors.length}>
+          {label}
+          {props.required && '*'}
+        </Label>
+        <Description error={!!errors.length}>{description}</Description>
+        {options.map(({label, value}, idx) => {
+          return (
+            <OptionContainer key={idx}>
+              <input
+                style={{margin: 0}}
+                type="radio"
+                value={value}
+                {...props}
+                ref={ref}
+              />
+              <OptionLabel>{label}</OptionLabel>
+            </OptionContainer>
+          );
+        })}
+        <Errors>{_errors}</Errors>
+      </Container>
+    );
+  },
+);
+
+Radio.displayName = 'Radio';
+
+export {Radio};
--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,33 +1,35 @@
-import React from 'react';
+import React, {forwardRef} from 'react';
 import {Container, Label, Description, Errors, BaseSelect} from './Select.sc';
 import {SelectProps} from './types';
 
-export const Select: React.FC<SelectProps> = ({
-  label,
-  description,
-  options,
-  errors = [],
-  ...props
-}) => {
-  const _errors = errors.map((error, idx) => <span key={idx}>• {error}</span>);
+const Select = forwardRef<HTMLSelectElement, SelectProps>(
+  ({label, description, options, errors = [], ...props}, ref) => {
+    const _errors = errors.map((error, idx) => (
+      <span key={idx}>• {error}</span>
+    ));
 
-  return (
-    <Container>
-      <Label htmlFor={props.name} error={!!errors.length}>
-        {label}
-        {props.required && '*'}
-      </Label>
-      <Description error={!!errors.length}>{description}</Description>
-      <BaseSelect {...props}>
-        {options.map(({label, value}, idx) => {
-          return (
-            <option key={idx} value={value}>
-              {label}
-            </option>
-          );
-        })}
-      </BaseSelect>
-      <Errors>{_errors}</Errors>
-    </Container>
-  );
-};
+    return (
+      <Container>
+        <Label htmlFor={props.name} error={!!errors.length}>
+          {label}
+          {props.required && '*'}
+        </Label>
+        <Description error={!!errors.length}>{description}</Description>
+        <BaseSelect {...props} ref={ref}>
+          {options.map(({label, value}, idx) => {
+            return (
+              <option key={idx} value={value}>
+                {label}
+              </option>
+            );
+          })}
+        </BaseSelect>
+        <Errors>{_errors}</Errors>
+      </Container>
+    );
+  },
+);
+
+Select.displayName = 'Select';
+
+export {Select};
