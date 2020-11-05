6b73c47fed000246fdffddda9e0f2a32be2332e7
Johannes Oster <johannes.oster@outlook.at>
2020-11-05 20:05:53 +0100

    Fix missing svg icons in build

--- rollup.config.js
+++ rollup.config.js
@@ -2,17 +2,19 @@ import typescript from 'rollup-plugin-typescript2';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import dts from 'rollup-plugin-dts';
 import pkg from './package.json';
+import svgr from '@svgr/rollup';
+import url from 'rollup-plugin-url';
 
 export default [
   {
     input: 'src/index.ts',
     output: [{file: pkg.main, format: 'cjs'}],
     output: [{file: pkg.module, format: 'esm'}],
-    plugins: [peerDepsExternal(), typescript()],
+    plugins: [peerDepsExternal(), typescript(), url(), svgr()],
   },
   {
     input: 'src/index.ts',
     output: [{file: 'dist/index.d.ts', format: 'es'}],
-    plugins: [dts()],
+    plugins: [dts(), url(), svgr()],
   },
 ];
